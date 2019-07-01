48711444c552c1a8d56382c01b3b14c89430922b
Austin Kang <austinkang14@gmail.com>
2019-07-01 15:53:24 -0700

    gifs

--- assets/JS/app.js
+++ assets/JS/app.js
@@ -1,76 +1,87 @@
-$(document).ready(function () {
-	var office = ["Michael Scott", "Dwight Schrute", "Jim Halpert"];
+$(document).ready(function(){
 
-	function newButtons() {
-		$("#office-buttons").empty();
-		for (i = 0; i < office.length; i++) {
-			$("#office-buttons").append("<button class='btn btn-success' office='" + office[i] + "'>" + office[i] + "</button>");
-		}
+	var chars = ["Michael Scott", "Jim Halpert"];
+
+	function showImgs(){
+
+			$("#office-img").empty();
+			var input = $(this).attr("data-name");
+			var limit = 10;
+			var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + input + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";;   
+
+			$.ajax({
+					url: queryURL, 
+					method: "GET"
+			}).done(function(response) {
+
+					for(var i = 0; i < limit; i++) {    
+
+							var displayDiv = $("<div>");
+							displayDiv.addClass("holder");
+					
+							var img = $("<img>");
+							img.attr("src", response.data[i].images.original_still.url);
+							img.attr("data-still", response.data[i].images.original_still.url);
+							img.attr("data-animate", response.data[i].images.original.url);
+							img.attr("data-state", "still");
+							img.attr("class", "gif");
+							displayDiv.prepend(img);
+
+							var rating = response.data[i].rating;
+							console.log(chars);
+							var pRating = $("<p>").text("Rating: " + rating);
+							displayDiv.prepend(pRating)
+
+							$("#office-img").prepend(displayDiv);
+					}
+			});
 	}
 
-	newButtons();
+	function renderButtons(){ 
 
-	
-	$("#add-office").on("click", function () {
-		event.preventDefault();
-		var offices = $("#office-input").val().trim();
-		office.push(offices);
-		renderButtons();
-		return;
-    });
-    $("button").on("click", function() {
-        var office = $(this).attr("data-person");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-          office + "&api_key=BkaUZZWcFij6J7AoQj3WtPb1R2p9O6V9&limit=10";
-  
-        $.ajax({
-          url: queryURL,
-          method: "GET"
-        })
-          .done(function(response) {
-            var results = response.data;
-  
-            for (var i = 0; i < results.length; i++) {
-              var gifDiv = $("<div>");
-  
-              var rating = results[i].rating;
-  
-              var p = $("<p>").text("Rating: " + rating);
-  
-              var offImage = $("<img>");
-              offImage.attr("src", results[i].images.fixed_height.url);
-              
-              offImage.attr("data-still", results[i].images.original_still.url);
-              offImage.attr("data-animate", results[i].images.original.url);
-              offImage.attr("data-state", "still");
-              offImage.attr("class", "gif");
-              gifDiv.prepend(p);
-              gifDiv.prepend(offImage);
-  
-              $("#offices").append(gifDiv);
-            }
-          });
-      });
+			$("#office-buttons").empty();
 
-	
-	
+			for (var i = 0; i < chars.length; i++){
 
-	function changeState(){
-		var state = $(this).attr("data-state");
-		var animateImage = $(this).attr("data-animate");
-		var stillImage = $(this).attr("data-still");
-
-		if (state == "still") {
-			$(this).attr("src", animateImage);
-			$(this).attr("data-state", "animate");
-		}
-
-		else if (state == "animate") {
-			$(this).attr("src", stillImage);
-			$(this).attr("data-state", "still");
-		}
+					var newButton = $("<button>") 
+					newButton.attr("class", "btn btn-default");
+					newButton.attr("id", "input")  
+					newButton.attr("data-name", chars[i]); 
+					newButton.text(chars[i]); 
+					$("#office-buttons").append(newButton); 
+			}
 	}
 
-	$(document).on("click", ".gif", changeState);
+	function imgState() {          
 
+			var state = $(this).attr("data-state");
+			var animateImage = $(this).attr("data-animate");
+			var stillImage = $(this).attr("data-still");
+
+			if(state == "still") {
+					$(this).attr("src", animateImage);
+					$(this).attr("data-state", "animate");
+			}
+
+			else if(state == "animate") {
+					$(this).attr("src", stillImage);
+					$(this).attr("data-state", "still");
+			}   
+	}
+
+	$("#officeadd").on("click", function(){
+
+			var input = $("#user-input").val().trim();
+			form.reset();
+			chars.push(input);
+							
+			renderButtons();
+
+			return false;
+	})
+
+	renderButtons();
+
+	$(document).on("click", "#input", showImgs);
+	$(document).on("click", ".gif", imgState);
 });
